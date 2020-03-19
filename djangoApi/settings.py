c0bb47959a60978f47b92a0127182ead507901d7
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-03-19 21:40:20 +0100

    url change

--- djangoApi/settings.py
+++ djangoApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["https://blooming-fortress-04444.herokuapp.com/"]
+ALLOWED_HOSTS = ["blooming-fortress-04444.herokuapp.com/"]
 
 if not DEBUG:
     CSRF_COOKIE_SECURE = True
