659423a4e3f02edc7880a5aabb557b9180c2721f
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-03-19 21:46:39 +0100

    hosts

--- djangoApi/settings.py
+++ djangoApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ["blooming-fortress-04444.herokuapp.com/"]
+ALLOWED_HOSTS = ["blooming-fortress-04444.herokuapp.com", "127.0.0.1"]
 
 if not DEBUG:
     CSRF_COOKIE_SECURE = True
