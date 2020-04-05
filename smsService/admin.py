fd9691e60e418890d0eb3fa165a4eb9bafa901df
Odd Eirik Igland <oddeirik.igland@gmail.com>
2020-04-05 19:28:51 +0200

    format

--- smsService/admin.py
+++ smsService/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from .models import PhoneBook
 
+
 class PhoneBookAdmin(admin.ModelAdmin):
-    list_display = ['name', 'phone_number']
+    list_display = ["name", "phone_number"]
+
 
 admin.site.register(PhoneBook, PhoneBookAdmin)
